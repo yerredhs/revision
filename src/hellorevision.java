de92d894d641b39bc3160a69f8e4407d4f804572
yerredhs <Saisanthosh.Yerredhu@cdk.com>
2019-07-11 15:16:48 +0530

    from intelllij

--- src/hellorevision.java
+++ src/hellorevision.java
@@ -1,6 +1,6 @@
 public class hellorevision {
     public static void main(String args[])
     {
-        System.out.println("Hello revision");
+        System.out.println("Hello revision changed from intellij");
     }
 }
